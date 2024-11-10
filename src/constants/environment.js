c53c7849ea3a384f811e2a050c7105854cd9a1c8
wmahmood1984 <56361582+wmahmood1984@users.noreply.github.com>
2024-11-10 11:45:29 +0500

    changes made

--- src/constants/environment.js
+++ src/constants/environment.js
@@ -8,7 +8,7 @@ import { bscTestnet } from "@wagmi/core/chains";
 import Web3 from "web3";
 
 export const presaleContract = {
-  address: "0xA1bc238DeAE92Bd00F73C39AD6Cc1436cB348aA4", //testnet
+  address: "0x076f4F1D04E88113B8F86ed6A6A712e26f1E7874", //testnet
   abi: presaleAbi,
 };
 export const airdropContract = {
